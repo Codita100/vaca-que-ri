db59a3afc6e6c6b849dded72d44b59c62ab0ba4e
Corina Enache <corinae@createdirect.ro>
2024-03-28 09:53:59 +0200

    users

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -18,6 +18,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::orderBy('created_at', 'desc')->get();
+        dd($users);
         return view('backend.users.index', compact('users', ));
     }
 
