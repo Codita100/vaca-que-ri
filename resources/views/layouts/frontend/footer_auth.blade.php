33770c546365f4c7c17744e96c634c9b7284d4c2
Corina Enache <corinae@createdirect.ro>
2024-04-17 11:56:36 +0300

    css

--- resources/views/layouts/frontend/footer_auth.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="row footer-links mx-auto">
-    <div class="col-md-4 mx-auto"><a href="/page/termos-e-condicoes" class="small_text"> TERMOS E CONDIÇÕES </a></div>
-    <div class="col-md-4"><a href="/page/politica-de-provacitade" class="small_text"> POLÍTICA DE PRIVACIDADE </a></div>
-    <div class="col-md-4 mx-auto"><a href="/page/politica-de-cookies" class="small_text"> POLÍTICA DE COOKIES </a></div>
-</div>
