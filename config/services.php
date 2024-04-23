396668f244efd504c1061ad763d495c4f1cdc9a8
Corina Enache <corinae@createdirect.ro>
2024-04-23 11:37:52 +0300

    all

--- config/services.php
+++ config/services.php
@@ -36,7 +36,7 @@ return [
     'google' => [
         'client_id'     => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect'      => "/google/callback",
+        'redirect'      => "https://lavacaquerie.createdev.ro/google/callback",
     ],
 
     'facebook' => [
