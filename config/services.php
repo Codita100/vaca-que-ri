640f0f9874d96ab347ea0464856285cb9bbf5550
Corina Enache <corinae@createdirect.ro>
2024-04-23 14:33:22 +0300

    google

--- config/services.php
+++ config/services.php
@@ -36,12 +36,12 @@ return [
     'google' => [
         'client_id'     => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect'      => env('GOOGLE_REDIRECT'),
+        'redirect'      => "/google/callback",
     ],
 
     'facebook' => [
         'client_id'     => env('FB_CLIENT_ID'),
         'client_secret' => env('FB_CLIENT_SECRET'),
-        'redirect'      => env('FB_REDIRECT'),
+        'redirect'      => "/facebook/callback",
     ],
 ];
