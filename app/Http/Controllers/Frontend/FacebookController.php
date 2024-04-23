bb16ad653eba5e3690394c2346627c7ffc0f8b80
Corina Enache <corinae@createdirect.ro>
2024-04-23 15:32:16 +0300

    google

--- app/Http/Controllers/Frontend/FacebookController.php
+++ app/Http/Controllers/Frontend/FacebookController.php
@@ -15,7 +15,7 @@ class FacebookController extends Controller
 {
     public function redirectToProvider($driver)
     {
-        return Socialite::driver($driver)->stateless()->redirect();
+        return Socialite::driver($driver)->stateless()->scopes(['email'])->user();
     }
 
     public function handleProviderCallback(Request $request, $driver)
